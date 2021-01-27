2e96f9038a125e396fc4dfcfc8abd3acee742ceb
Bruno Fontana <bruno.fontana@viasoft.com.br>
2021-01-27 13:17:30 -0300

    Cria class util e cria metodos para criar o elemento video

---
+++ pages/room/src/view.js
@@ -0,0 +1,34 @@
+class View {
+  constructor(){
+
+  }
+
+  createVideoElement({ muted = true, src, srcObject }) {
+    const video = document.createElement('video');
+    video.muted = muted;
+    video.src = src;
+    video.srcObject = srcObject;
+
+    if (src) {
+      video.controls = true;
+      video.loop = true;
+      Util.sleep(200).then(_ => video.play());
+    }
+
+    if (srcObject) {
+      video.addEventListener('loadedmetadata', _ => video.play());
+    }
+
+    return video;
+  }
+
+
+  renderVideo({ userId, stream = null, url = null }) {
+    const video = this.createVideoElement({ src: url, srcObject: stream });
+  }
+
+  appendToHTMLTree(userId, video) {
+    // To:do: apendar no html
+
+  }
+}
